f7b2048a612c524abf059878d2f0694d9eeac5e4
Rameen Rastan <rameenrastanv@hotmail.com>
2017-11-20 15:18:26 -0500

    added argparse so user can specify image whose shape will be predicted, cleaned up code

--- identifier.py
+++ identifier.py
@@ -7,30 +7,40 @@ import cv2
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from pathlib import Path
 from tflearn.layers.estimator import regression
-
-# import matplotlib.pyplot as plt
+import argparse
 
 # shape labels
 square = [1, 0, 0, 0]
 circle = [0, 1, 0, 0]
 triangle = [0, 0, 1, 0]
 star = [0, 0, 0, 1]
-img_size = 35
-LR = 1e-3
-MODEL_NAME = 'shapeClassifer-{}-{}.model'.format(LR, '2conv-basic')
 
-TRAIN_DIR = 'data/'
-SQUARE_DIR = TRAIN_DIR + "square/"
-CIRCLE_DIR = TRAIN_DIR + "circle/"
-STAR_DIR = TRAIN_DIR + "star/"
-TRIANGLE_DIR = TRAIN_DIR + "triangle/"
+#specifies image size (images get transformed to this size during preprocessing)
+image_size = 35
 
+#name of our convolutional neural network model when saving as file
+model = 'shapeClassifer'
+
+#dataset directory
+dataset_directory = 'data/'
+#shape dataset directories
+square_directory = dataset_directory + "square/"
+circle_directory = dataset_directory + "circle/"
+star_directory = dataset_directory + "star/"
+triangle_directory = dataset_directory + "triangle/"
+
+#allows user to input an image in the command line using --image [image path]
+def parse(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=str)
+    arg = parser.parse_args()
+    return arg.image
 
 # preprocessing module (normalizes image size and shape to square, converts image color to grayscale)
 def preprocess_image(image):
     image = str(image)
     image = cv2.imread(image)
-    image = cv2.resize(image, (img_size, img_size))
+    image = cv2.resize(image, (image_size, image_size))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return image
 
@@ -45,34 +55,31 @@ def extract_features(image):
 # creates training data through preprocessing and feature extraction of training dataset
 def create_labeled_feature_dataset():
     feature_training_dataset = []
-    for image in os.listdir(SQUARE_DIR):
-        image = SQUARE_DIR + str(image)
+    for image in os.listdir(square_directory):
+        image = square_directory + str(image)
         features = extract_features(image)
         feature_training_dataset.append([features, np.array(square)])
-    for image in os.listdir(CIRCLE_DIR):
-        image = CIRCLE_DIR + str(image)
+    for image in os.listdir(circle_directory):
+        image = circle_directory + str(image)
         features = extract_features(image)
         feature_training_dataset.append([features, np.array(circle)])
-    for image in os.listdir(STAR_DIR):
-        image = STAR_DIR + str(image)
+    for image in os.listdir(star_directory):
+        image = star_directory + str(image)
         features = extract_features(image)
         feature_training_dataset.append([features, np.array(star)])
-    for image in os.listdir(TRIANGLE_DIR):
-        image = TRIANGLE_DIR + str(image)
+    for image in os.listdir(triangle_directory):
+        image = triangle_directory + str(image)
         features = extract_features(image)
         feature_training_dataset.append([features, np.array(triangle)])
-
     np.random.shuffle(feature_training_dataset)
     return feature_training_dataset
 
-
+#create our feature training dataset
 training_feature_dataset = create_labeled_feature_dataset()
-np.save('training_feature_dataset.npy', training_feature_dataset)
-
 
 # define our convolutional neural network model using tensorflow and tflearn
 def create_cnn_model():
-    cnn = input_data(shape=[None, img_size, img_size, 1], name="input")
+    cnn = input_data(shape=[None, image_size, image_size, 1], name="input")
     cnn = conv_2d(cnn, 32, 2, activation="relu")
     cnn = max_pool_2d(cnn, 2)
     cnn = conv_2d(cnn, 64, 2, activation="relu")
@@ -96,40 +103,43 @@ def create_cnn_model():
     cnn = regression(cnn, optimizer="adam", learning_rate=0.001, loss="categorical_crossentropy")
     return cnn
 
-
-# instantiate our model
+# instantiate our model using tflearn
 cnn = tflearn.DNN(create_cnn_model(), tensorboard_dir="cnn")
 
-if os.path.exists('{}.meta'.format(MODEL_NAME)):
-    cnn.load(MODEL_NAME)
-    print('model loaded!')
+#if our trained model already exists, load it
+if os.path.exists(model + '.meta'):
+    cnn.load(model)
+#if our trained model doesn't exist, train the model and save it    
 else:
-    # checks if modeled was already trained and saved
-    training_data = training_feature_dataset[:-500]
-    testing_data = training_feature_dataset[-500:]
+    training_data = training_feature_dataset[:-250]
+    testing_data = training_feature_dataset[-250:]
 
-    x = np.array([i[0] for i in training_data]).reshape(-1, img_size, img_size, 1)
+    x = np.array([i[0] for i in training_data]).reshape(-1, image_size, image_size, 1)
     y = np.array([i[1] for i in training_data])
 
-    test_x = np.array([i[0] for i in testing_data]).reshape(-1, img_size, img_size, 1)
+    test_x = np.array([i[0] for i in testing_data]).reshape(-1, image_size, image_size, 1)
     test_y = np.array([i[1] for i in testing_data])
 
-    cnn.fit(x, y, n_epoch=3, validation_set=(test_x, test_y), snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+    cnn.fit(x, y, n_epoch=3, validation_set=(test_x, test_y), snapshot_step=500, show_metric=True, run_id=model)
 
-    cnn.save(MODEL_NAME)
+    cnn.save(model)
 
-image = 'data/circle/1.png'
-image_features = extract_features(image)
-data = image_features.reshape(35, 35, 1)
-prediction = cnn.predict([data])
+#input our trained model and image, and prints our the prediction of what shape the image is
+def make_prediction(model, image):
+    image_features = extract_features(image)
+    image_features = image_features.reshape(35, 35, 1)
+    prediction = model.predict([image_features])
+    if (np.argmax(prediction) == 0):
+        print('This is a square!')
+    elif (np.argmax(prediction) == 1):
+        print('This is a circle!')
+    elif (np.argmax(prediction) == 2):
+        print('This is a triangle!')
+    elif (np.argmax(prediction) == 3):
+        print('This is a star!')
+    else:
+        print('This is not a square, circle, triangle or square!')
 
-if (np.argmax(prediction) == 0):
-    print('This is a square!')
-elif (np.argmax(prediction) == 1):
-    print('This is a circle!')
-elif (np.argmax(prediction) == 2):
-    print('This is a triangle!')
-elif (np.argmax(prediction) == 3):
-    print('This is a star!')
-else:
-    print('This is not a square, circle, triangle or square!')
+#make prediction based on the image the user specified in command line (using --image [image path])
+image = parse()
+make_prediction(cnn, image)        
